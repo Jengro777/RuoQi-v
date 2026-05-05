f57f53c5958db553fcae13e3c99a4ae454883a83
Jengro <avey777@outlook.com>
2026-05-05 09:58:51 +0200

    Update route_sys_admin.v (#271)
    Reviewed-on: https://codeberg.org/RuoQi/RuoQi-v/pulls/271

--- backend/route/route_sys_admin.v
+++ backend/route/route_sys_admin.v
@@ -2,7 +2,7 @@ module route
 
 import log
 import structs { Context }
-import handler.rest.sys_admin as user_ddd
+// import handler.rest.sys_admin as user_ddd
 import service.sys_admin_api { Admin } // 必须是路由模块内部声明的结构体
 import service.sys_admin_api.user { User }
 import service.sys_admin_api.token { Token }
@@ -22,8 +22,8 @@ fn (mut app AliasApp) routes_sys_admin(mut ctx Context) {
 	log.debug('${@METHOD}  ${@MOD}.${@FILE_LINE}')
 	// 方式二：通过泛型的方式使用全局中间件，适合对多个控制器使用相同的中间件
 
-	app.register_routes_no_auth[user_ddd.User, Context](mut &user_ddd.User{},
-		'/sys_admin/ddd/user', mut ctx)
+	// app.register_routes_no_auth[user_ddd.User, Context](mut &user_ddd.User{},
+	// 	'/sys_admin/ddd/user', mut ctx)
 	// 不需要token_jwt 认证
 	app.register_routes_no_auth[Authentication, Context](mut &Authentication{}, '/sys_admin/auth', mut
 		ctx)
